7578fa83e6290b2e6b4349e3dc4fdaab41e69dc9
HalfAdd3r <pconant@gmail.com>
2014-02-23 11:05:50 -0500

    Major Changes to UI and data structure
    Now that structure is forming up, I was able to add fill-in values to
    rest of UI.  Next to finish dynamically building FedEx IO

--- js/background.js
+++ js/background.js
@@ -2,7 +2,7 @@
 chrome.app.runtime.onLaunched.addListener(function(){
 	chrome.app.window.create('window.html',{
 		'bounds':{
-			'width':640,
+			'width':940,
 			'height':500
 		}
 	});
--- js/fileHandler.js
+++ js/fileHandler.js
@@ -11,35 +11,41 @@ angular.module('fileHandler', []).
         var filehandler = {};
 
         function parseCSVtoJSON (rawText, scope) {
-            var mainDataObj = [];
-            var header = [];
-            //var index = 1;
+            var index = 1;
             
             var lines = rawText.split('\n');
-            header = lines[0].split(',');
-
+            
 
             // Iterate over file ignoring first line - only handles upwards of 100 records
             $(lines).slice(1,101).each(function(){
-                var tempAddress = [];
                 var innerLines = this.split(',');
 
                 // LATER write fancy loop to create dynamically named CSV headers
-                tempAddress = ({
-                    "name":innerLines[0],
-                    "company":innerLines[1],
-                    "address1":innerLines[2],
-                    "address2":innerLines[3],
-                    "city":innerLines[4],
-                    "state":innerLines[5],
-                    "zip":innerLines[6],
-                    "country":innerLines[7],
-                    "resi":innerLines[8]
-                });
+                tempAddress = [{
+                    name:innerLines[0],
+                    company:innerLines[1],
+                    address1:innerLines[2],
+                    address2:innerLines[3],
+                    city:innerLines[4],
+                    state:innerLines[5],
+                    zip:innerLines[6],
+                    country:innerLines[7],
+                    resi:innerLines[8]
+                }];
 
-                scope.addresses.push(tempAddress);
+                console.log(tempAddress);
 
-                //index++;
+                newAddress = {
+                    index:index,
+                    addvalues:tempAddress,
+                    checkvalues:tempAddress
+                };
+
+                console.log(newAddress);
+
+                scope.addresses.push(newAddress);
+
+                index++;
             });
             scope.$apply();
         }; // end parseCSVtoJSON
